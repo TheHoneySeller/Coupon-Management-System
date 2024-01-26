4072e6dff990d8417ea4494235d07fc756685aad
Faruk Balci <farukbalci99@gmail.com>
2024-01-26 20:06:53 +0300

    Init project.

---
+++ backend/src/test/java/com/faruk/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faruk.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
