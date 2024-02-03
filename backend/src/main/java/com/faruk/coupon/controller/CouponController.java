60e1e2c6098ab4dd837f888f07818f66bcca2191
Faruk Balci <faruk@192.168.1.62>
2024-02-04 02:00:14 +0300

    Setup file logging. Update .gitignore

--- backend/src/main/java/com/faruk/coupon/controller/CouponController.java
+++ backend/src/main/java/com/faruk/coupon/controller/CouponController.java
@@ -2,6 +2,8 @@ package com.faruk.coupon.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,12 @@ import com.faruk.coupon.constants_enums.ConstantsEnums.CouponType;
 import com.faruk.coupon.model.Coupon;
 import com.faruk.coupon.service.CouponService;
 
+
 @Controller
 public class CouponController {
     private final CouponService couponService;
     //private final LanguageProvider language; // to do: implement and use :D
+    private Logger logger = LoggerFactory.getLogger(CouponController.class);
 
     public CouponController(CouponService couponService) {
         this.couponService = couponService;
@@ -27,6 +31,7 @@ public class CouponController {
 
     @PostMapping("/bulkcouponupload")
     public ResponseEntity<String> bulkCouponUpload(@RequestBody List<Coupon> coupons) {
+        logger.info("info mf");
         if (coupons == null || coupons.size() > ConstantsEnums.MAX_COUPON_UPLOAD_NUMBER) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Coupons is null or there are too many coupons to upload. Max: " + ConstantsEnums.MAX_COUPON_UPLOAD_NUMBER);
         } else {
